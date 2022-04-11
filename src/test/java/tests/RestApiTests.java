3ad818d6d4168511a8db038ad1cd448261385ff2
AlexGeorgievich <alexhome1@yandex.ru>
2022-04-11 17:54:42 +0300

    Added test on email ends

--- src/test/java/tests/RestApiTests.java
+++ src/test/java/tests/RestApiTests.java
@@ -1,11 +1,15 @@
 package tests;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RestApiTests {
 
@@ -42,12 +46,24 @@ public class RestApiTests {
         String authorisedData = "{ \"email\": \"sydney@fife\" }";
         given()
                 .body(authorisedData)
-                .contentType(JSON)
+                .contentType(ContentType.JSON)
                 .when()
                 .post("https://reqres.in/api/register")
                 .then()
                 .statusCode(400)
                 .body("error",is("Missing password"));
+    }
+
+    @Test
+    void chechAvatatAndIdTest() {
+        List<UserData> users = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get("https://reqres.in/api/users?page=2")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+// все email оканчиваются на @reqres.in
+        assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")));
     }
 
 }
